5543d11fc95609253b1d73f3ceead8d9c07ee781
Grigorii Turkin <turick7@yandex.ru>
2022-06-27 23:52:31 +0300

    change install_ruby.sh

--- install_ruby.sh
+++ install_ruby.sh
@@ -1,4 +1,2 @@
-sudo apt update
-sudo apt install -y ruby-full ruby-bundler build-essential
-ruby -v
-bundler -v
+#!/bin/sh
+sudo apt update && sudo apt install -y ruby-full ruby-bundler build-essential
